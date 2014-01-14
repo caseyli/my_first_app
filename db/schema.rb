c1ae9fb12ff00f452211355a374f9785b5f822bc
Casey Li <casey.li@gmail.com>
2014-01-13 21:11:20 -0500

    Transferred to postgres.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140114000815) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "news_posts", force: true do |t|
     t.string   "title"
     t.text     "body"
