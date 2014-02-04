c98643131547dca2b489b23811e26cfb01984ca6
Casey Li <casey.li@gmail.com>
2014-02-03 21:20:45 -0500

    Finished off News Post resource and added some styling.

--- app/controllers/news_posts_controller.rb
+++ app/controllers/news_posts_controller.rb
@@ -24,6 +24,23 @@ class NewsPostsController < ApplicationController
   end
 
   def edit
+    @news_post = NewsPost.find(params[:id])
+  end
+
+  def update
+    @news_post = NewsPost.find(params[:id])
+
+    if @news_post.update(news_post_params)
+      redirect_to "/news_posts/#{@news_post.id}"
+    else
+      # DO SOMETHING ELSE
+    end
+  end
+
+  def destroy
+    @news_post = NewsPost.find(params[:id])
+    @news_post.destroy
+    redirect_to "/news_posts"
   end
 
   private
