6de0c5fb165ebeef59806e3e2753ebd6b9f253fc
Casey Li <casey.li@gmail.com>
2014-03-03 20:33:04 -0500

    Added ResumeEntries and authorization.

--- app/controllers/news_posts_controller.rb
+++ app/controllers/news_posts_controller.rb
@@ -1,6 +1,6 @@
 class NewsPostsController < ApplicationController
 
-  before_action :authorize_for_news_posts, except: [:index, :show]
+  before_action :deny_access_for_non_admins, except: [:index, :show]
 
   def index
   	@news_posts = NewsPost.all
@@ -45,10 +45,6 @@ class NewsPostsController < ApplicationController
   end
 
   private
-
-    def authorize_for_news_posts
-      redirect_to root_path if current_user.blank?
-    end
 
     def news_post_params
       params.require(:news_post).permit(:title, :body)
